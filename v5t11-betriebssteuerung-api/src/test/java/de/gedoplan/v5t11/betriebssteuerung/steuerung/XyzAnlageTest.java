79596c19544a96d9fd061e16b89927a6843b34ba
moba <dirk.weil@web.de>
2015-01-17 23:32:53 +0100

    W5 und W6 in Niederbach mit Gleis 3 kombiniert

--- v5t11-betriebssteuerung-api/src/test/java/de/gedoplan/v5t11/betriebssteuerung/steuerung/XyzAnlageTest.java
+++ v5t11-betriebssteuerung-api/src/test/java/de/gedoplan/v5t11/betriebssteuerung/steuerung/XyzAnlageTest.java
@@ -34,7 +34,7 @@ public abstract class XyzAnlageTest
   }
 
   @Test
-  @Ignore
+//  @Ignore
   public void showGleiseOhneBesetztmelder()
   {
     for (Gleisabschnitt gleisabschnitt : this.steuerung.getGleisabschnitte())
@@ -48,7 +48,7 @@ public abstract class XyzAnlageTest
   }
 
   @Test
-  @Ignore
+//  @Ignore
   public void showFreieBesetztmelder()
   {
     for (Besetztmelder besetztmelder : this.steuerung.getBesetztmelder())
