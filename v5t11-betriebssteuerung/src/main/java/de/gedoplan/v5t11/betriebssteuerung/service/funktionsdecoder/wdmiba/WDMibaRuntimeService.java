cd84d5bdb5c5c82fa434b59f26b9df82f5e91269
Dirk Weil <dirk.weil@gedoplan.de>
2017-01-03 20:00:36 +0100

    WDMiba wieder hinzugefügt

--- v5t11-betriebssteuerung/src/main/java/de/gedoplan/v5t11/betriebssteuerung/service/funktionsdecoder/wdmiba/WDMibaRuntimeService.java
+++ v5t11-betriebssteuerung/src/main/java/de/gedoplan/v5t11/betriebssteuerung/service/funktionsdecoder/wdmiba/WDMibaRuntimeService.java
@@ -1,33 +1,54 @@
 package de.gedoplan.v5t11.betriebssteuerung.service.funktionsdecoder.wdmiba;
 
+import de.gedoplan.v5t11.betriebssteuerung.entity.BausteinConfiguration;
+import de.gedoplan.v5t11.betriebssteuerung.service.BausteinConfigurationService;
 import de.gedoplan.v5t11.betriebssteuerung.service.ConfigurationRuntimeService;
+import de.gedoplan.v5t11.betriebssteuerung.service.Current;
+import de.gedoplan.v5t11.betriebssteuerung.service.Programmierfamilie;
+import de.gedoplan.v5t11.betriebssteuerung.steuerung.baustein.Baustein;
+import de.gedoplan.v5t11.betriebssteuerung.steuerung.baustein.Funktionsdecoder;
+import de.gedoplan.v5t11.betriebssteuerung.steuerung.baustein.funktionsdecoder.WDMiba;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
+
+import lombok.Getter;
 
 /**
  * Service zum Lesen und Schreiben der Konfiguration eines Funktionsdecoders des Typs WD-MIBA.
  *
  * @author dw
  */
-@ApplicationScoped
-public class WDMibaRuntimeService extends ConfigurationRuntimeService<WDMibaConfigurationAdapter> {
+@ConversationScoped
+@Programmierfamilie(WDMiba.class)
+public class WDMibaRuntimeService extends ConfigurationRuntimeService {
+  @Getter
+  private WDMibaConfigurationAdapter configuration;
+
+  @Inject
+  public WDMibaRuntimeService(@Current Baustein baustein, BausteinConfigurationService bausteinConfigurationService) {
+    BausteinConfiguration bausteinSollConfiguration = bausteinConfigurationService.getBausteinConfiguration(baustein);
+    BausteinConfiguration bausteinIstConfiguration = new BausteinConfiguration(baustein.getId());
+    this.configuration = new WDMibaConfigurationAdapter((Funktionsdecoder) baustein, bausteinIstConfiguration, bausteinSollConfiguration);
+  }
+
   @Override
-  public void getRuntimeValues(WDMibaConfigurationAdapter configuration) {
-    configuration.setAdresseIst(this.selectrixGateway.getValue(0));
+  public void getRuntimeValues() {
+    this.configuration.setAdresseIst(this.selectrixGateway.getValue(0));
 
     int betriebsArt = this.selectrixGateway.getValue(1);
     for (int i = 0, bit = 1; i < 8; ++i, bit <<= 1) {
-      configuration.getDauer()[i].setIst((betriebsArt & bit) != 0);
+      this.configuration.getDauer()[i].setIst((betriebsArt & bit) != 0);
     }
   }
 
   @Override
-  public void setRuntimeValues(WDMibaConfigurationAdapter configuration) {
-    this.selectrixGateway.setValue(0, configuration.getAdresseIst());
+  public void setRuntimeValues() {
+    this.selectrixGateway.setValue(0, this.configuration.getAdresseIst());
 
     int betriebsArt = 0;
     for (int i = 0, bit = 1; i < 8; ++i, bit <<= 1) {
-      if (configuration.getDauer()[i].getIst()) {
+      if (this.configuration.getDauer()[i].getIst()) {
         betriebsArt |= bit;
       }
     }
