9602022ebb52e66a30fc5039d830562d275e33a0
Dirk Weil <dirk.weil@gedoplan.de>
2017-01-04 21:17:39 +0100

    Programmierung von D&H-Lokdecodern ergänzt

--- v5t11-betriebssteuerung/src/main/java/de/gedoplan/v5t11/betriebssteuerung/service/lokdecoder/dhlokdecoder/DHLokdecoderRuntimeService.java
+++ v5t11-betriebssteuerung/src/main/java/de/gedoplan/v5t11/betriebssteuerung/service/lokdecoder/dhlokdecoder/DHLokdecoderRuntimeService.java
@@ -2,9 +2,10 @@ package de.gedoplan.v5t11.betriebssteuerung.service.lokdecoder.dhlokdecoder;
 
 import de.gedoplan.v5t11.betriebssteuerung.entity.BausteinConfiguration;
 import de.gedoplan.v5t11.betriebssteuerung.service.BausteinConfigurationService;
-import de.gedoplan.v5t11.betriebssteuerung.service.ConfigurationRuntimeService;
 import de.gedoplan.v5t11.betriebssteuerung.service.Current;
 import de.gedoplan.v5t11.betriebssteuerung.service.Programmierfamilie;
+import de.gedoplan.v5t11.betriebssteuerung.service.lokdecoder.LokdecoderRuntimeService;
+import de.gedoplan.v5t11.betriebssteuerung.service.lokdecoder.dhlokdecoder.DHLokdecoderConfigurationAdapter.RegelVariante;
 import de.gedoplan.v5t11.betriebssteuerung.steuerung.baustein.Baustein;
 import de.gedoplan.v5t11.betriebssteuerung.steuerung.baustein.Lokdecoder;
 import de.gedoplan.v5t11.betriebssteuerung.steuerung.baustein.lokdecoder.DHLokdecoder;
@@ -12,14 +13,9 @@ import de.gedoplan.v5t11.betriebssteuerung.steuerung.baustein.lokdecoder.DHLokde
 import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
 
-import lombok.Getter;
-
 @ConversationScoped
 @Programmierfamilie(DHLokdecoder.class)
-public class DHLokdecoderRuntimeService extends ConfigurationRuntimeService {
-  @Getter
-  private DHLokdecoderConfigurationAdapter configuration;
-
+public class DHLokdecoderRuntimeService extends LokdecoderRuntimeService<DHLokdecoderConfigurationAdapter> {
   @Inject
   public DHLokdecoderRuntimeService(@Current Baustein baustein, BausteinConfigurationService bausteinConfigurationService) {
     BausteinConfiguration bausteinSollConfiguration = bausteinConfigurationService.getBausteinConfiguration(baustein);
@@ -29,12 +25,50 @@ public class DHLokdecoderRuntimeService extends ConfigurationRuntimeService {
 
   @Override
   public void getRuntimeValues() {
-    this.configuration.setAdresseIst(this.selectrixGateway.getValue(0));
+    startProgMode();
+
+    try {
+      // Standardparameter holen
+      pullStandardConfig();
+
+      try {
+        // Decoder auf Lesen erweiterter Parameter umprogrammieren
+        writeDecoderDaten(0x0000_0000_0100_1001);
+
+        int erweiterteDecoderDaten = readDecoderDaten();
+
+        this.configuration.getAnschlusstauschMotor().setIst((erweiterteDecoderDaten & 0b0000_0001) != 0);
+        this.configuration.getAnschlusstauschLicht().setIst((erweiterteDecoderDaten & 0b0000_0010) != 0);
+        this.configuration.getAnschlusstauschGleis().setIst((erweiterteDecoderDaten & 0b0000_0100) != 0);
+        this.configuration.getRegelVariante().setIst(RegelVariante.valueOf((erweiterteDecoderDaten & 0b1100_0000) >>> 6));
+      } finally {
+        // Standardparameter restaurieren
+        pushStandardConfig();
+      }
+    } finally {
+      stopProgMode();
+    }
   }
 
   @Override
   public void setRuntimeValues() {
-    this.selectrixGateway.setValue(0, this.configuration.getAdresseIst());
-  }
+    startProgMode();
 
+    try {
+      // Decoder auf Schreiben erweiterter Parameter umprogrammieren
+      writeDecoderDaten(0x0000_0000_0100_1001);
+
+      // Erweiterte Parameter schreiben
+      int erweiterteDecoderDaten = (this.configuration.getAnschlusstauschMotor().getIst() ? 0b0000_0001 : 0)
+          | (this.configuration.getAnschlusstauschLicht().getIst() ? 0b0000_0010 : 0)
+          | (this.configuration.getAnschlusstauschGleis().getIst() ? 0b0000_0100 : 0)
+          | ((this.configuration.getRegelVariante().getIst().getBits() << 6) & 0b1100_0000);
+      writeDecoderDaten(erweiterteDecoderDaten);
+
+      // Standardparameter schreiben
+      pushStandardConfig();
+    } finally {
+      stopProgMode();
+    }
+  }
 }
