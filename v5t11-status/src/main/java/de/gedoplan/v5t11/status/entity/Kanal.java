08547e06c928a7a796dff5770db419da98c9c96a
Dirk Weil <dirk.weil@gedoplan.de>
2018-08-13 13:03:43 +0200

    JSON-Binding von Kanal gefixt

--- v5t11-status/src/main/java/de/gedoplan/v5t11/status/entity/Kanal.java
+++ v5t11-status/src/main/java/de/gedoplan/v5t11/status/entity/Kanal.java
@@ -1,5 +1,7 @@
 package de.gedoplan.v5t11.status.entity;
 
+import de.gedoplan.v5t11.status.jsonb.JsonbInclude;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -9,9 +11,11 @@ import lombok.Getter;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
-@Getter
 @AllArgsConstructor
 public class Kanal {
+  @Getter(onMethod = @__(@JsonbInclude))
   private int adresse;
+
+  @Getter(onMethod = @__(@JsonbInclude))
   private int wert;
 }
