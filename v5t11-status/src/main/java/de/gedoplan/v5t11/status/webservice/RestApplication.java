2e1e0049cfe82fec12a07c714b0e5f1379b264b9
Dirk Weil <dirk.weil@gedoplan.de>
2018-08-22 23:38:07 +0200

    URL verkürzt

--- v5t11-status/src/main/java/de/gedoplan/v5t11/status/webservice/RestApplication.java
+++ v5t11-status/src/main/java/de/gedoplan/v5t11/status/webservice/RestApplication.java
@@ -3,6 +3,6 @@ package de.gedoplan.v5t11.status.webservice;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("rest")
+@ApplicationPath("rs")
 public class RestApplication extends Application {
 }
