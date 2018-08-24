566018fa2f0596c85aa5803ea663a81f4090e133
Dirk Weil <dirk.weil@gedoplan.de>
2018-08-24 21:32:06 +0200

    v5t11-strecken entfernt

--- v5t11-strecken/src/main/java/de/gedoplan/v5t11/strecken/webservice/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package de.gedoplan.v5t11.strecken.webservice;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("rest")
-public class RestApplication extends Application {
-}
