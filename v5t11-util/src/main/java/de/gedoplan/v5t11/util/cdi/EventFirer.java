beb7cfc02a31b9ba723f14b3227fff70ad7a4484
Dirk Weil <dirk.weil@gedoplan.de>
2018-08-26 12:56:31 +0200

    Events ggf. mit Qualifiern feuern und auch asynchron

--- v5t11-util/src/main/java/de/gedoplan/v5t11/util/cdi/EventFirer.java
+++ v5t11-util/src/main/java/de/gedoplan/v5t11/util/cdi/EventFirer.java
@@ -1,17 +1,27 @@
 package de.gedoplan.v5t11.util.cdi;
 
-import javax.enterprise.inject.spi.BeanManager;
+import java.lang.annotation.Annotation;
+
+import javax.enterprise.event.Event;
 import javax.enterprise.inject.spi.CDI;
+import javax.enterprise.util.TypeLiteral;
 
 public final class EventFirer {
-  private static BeanManager beanManager = null;
+  private static Event<Object> eventSource = null;
 
-  public static void fire(Object event) {
-    if (beanManager == null) {
-      beanManager = CDI.current().select(BeanManager.class).get();
+  public static void fire(Object event, Annotation... qualifiers) {
+    if (eventSource == null) {
+      eventSource = CDI.current()
+          .select(new TypeLiteral<Event<Object>>() {})
+          .get();
     }
 
-    beanManager.fireEvent(event);
+    Event<Object> es = eventSource;
+    if (qualifiers.length != 0) {
+      es = es.select(qualifiers);
+    }
+    es.fire(event);
+    es.fireAsync(event);
   }
 
   private EventFirer() {
