da49a7115dc699894b6d37bdf57a24bc1b722d23
Dirk Weil <dirk.weil@gedoplan.de>
2018-08-26 18:01:42 +0200

    FULL-Provider registrieren

--- v5t11-fahrstrassen/src/main/java/de/gedoplan/v5t11/fahrstrassen/webservice/RestApplication.java
+++ v5t11-fahrstrassen/src/main/java/de/gedoplan/v5t11/fahrstrassen/webservice/RestApplication.java
@@ -1,4 +1,4 @@
-package de.gedoplan.v5t11.status.webservice;
+package de.gedoplan.v5t11.fahrstrassen.webservice;
 
 import de.gedoplan.v5t11.util.webservice.provider.JsonMessageBodyReader;
 import de.gedoplan.v5t11.util.webservice.provider.JsonMessageBodyWriter;
@@ -7,9 +7,11 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.enterprise.context.Dependent;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@Dependent
 @ApplicationPath("rs")
 public class RestApplication extends Application {
 
@@ -17,11 +19,7 @@ public class RestApplication extends Application {
   public Set<Class<?>> getClasses() {
     return Stream.of(
         // Services
-        GleisResource.class,
-        KanalResource.class,
-        LokResource.class,
-        SignalResource.class,
-        WeicheResource.class,
+        FahrstrasseResource.class,
 
         // Provider
         JsonMessageBodyReader.FULL.class,
