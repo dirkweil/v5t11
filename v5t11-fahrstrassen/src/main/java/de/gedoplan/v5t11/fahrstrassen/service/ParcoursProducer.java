959264c53c940b80326cf6c9542ce652eda11ad3
Dirk Weil <dirk.weil@gedoplan.de>
2018-10-16 22:22:06 +0200

    .

--- v5t11-fahrstrassen/src/main/java/de/gedoplan/v5t11/fahrstrassen/service/ParcoursProducer.java
+++ v5t11-fahrstrassen/src/main/java/de/gedoplan/v5t11/fahrstrassen/service/ParcoursProducer.java
@@ -1,9 +1,6 @@
 package de.gedoplan.v5t11.fahrstrassen.service;
 
 import de.gedoplan.v5t11.fahrstrassen.entity.Parcours;
-import de.gedoplan.v5t11.fahrstrassen.gateway.GleisResourceClient;
-import de.gedoplan.v5t11.fahrstrassen.gateway.SignalResourceClient;
-import de.gedoplan.v5t11.fahrstrassen.gateway.WeicheResourceClient;
 import de.gedoplan.v5t11.util.cdi.Created;
 import de.gedoplan.v5t11.util.cdi.EventFirer;
 
@@ -15,7 +12,7 @@ public class ParcoursProducer {
 
   @Produces
   @ApplicationScoped
-  Parcours createParcours(ConfigService configService, GleisResourceClient gleisResourceClient, SignalResourceClient signalResourceClient, WeicheResourceClient weicheResourceClient) {
+  Parcours createParcours(ConfigService configService) {
 
     Parcours parcours = configService.readXmlConfig("_parcours.xml", Parcours.class);
 
