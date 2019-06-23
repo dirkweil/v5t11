467a665e943b7243f41ac990811342dd056d6573
Dirk Weil <dirk.weil@gedoplan.de>
2019-06-23 13:37:23 +0200

    Exception-Klasse ergänzt

---
+++ v5t11-util/src/main/java/de/gedoplan/v5t11/util/misc/V5t11Exception.java
@@ -0,0 +1,13 @@
+package de.gedoplan.v5t11.util.misc;
+
+public class V5t11Exception extends RuntimeException {
+
+  public V5t11Exception(String message) {
+    super(message);
+  }
+
+  public V5t11Exception(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+}
