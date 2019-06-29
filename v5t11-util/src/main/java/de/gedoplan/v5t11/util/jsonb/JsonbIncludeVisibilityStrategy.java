096462c678cb5f9b747aec0884207d6bcaedc395
Dirk Weil <dirk.weil@gedoplan.de>
2019-06-29 11:03:34 +0200

    Abstrakte Methoden ignorieren

--- v5t11-util/src/main/java/de/gedoplan/v5t11/util/jsonb/JsonbIncludeVisibilityStrategy.java
+++ v5t11-util/src/main/java/de/gedoplan/v5t11/util/jsonb/JsonbIncludeVisibilityStrategy.java
@@ -2,6 +2,7 @@ package de.gedoplan.v5t11.util.jsonb;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 import javax.json.bind.config.PropertyVisibilityStrategy;
 
@@ -21,7 +22,25 @@ public class JsonbIncludeVisibilityStrategy implements PropertyVisibilityStrateg
 
   @Override
   public boolean isVisible(Method method) {
+    if ((method.getModifiers() & Modifier.ABSTRACT) != 0) {
+      return false;
+    }
+
     JsonbInclude jsonBInclude = method.getAnnotation(JsonbInclude.class);
+
+    // Suche nach dem unerklärlichen Phänomen, dass getId manchmal keine Annotationen hat (scheinbar nur wenn Yasson genutzt wird ...
+    // if ("getId".equals(method.getName())) {
+    // System.out.println("### method: " + method);
+    // System.out.println(" ### jsonBInclude: " + jsonBInclude);
+    // System.out.println(" ### dito, declared: " + method.getDeclaredAnnotation(JsonbInclude.class));
+    // System.out.println(" ### declaringClass: " + method.getDeclaringClass());
+    // System.out.println(" ### annotations: " + Stream.of(method.getAnnotations()).map(a -> a.toString()).collect(Collectors.joining(",")));
+    //
+    // if (jsonBInclude == null) {
+    // return false;
+    // }
+    // }
+
     return jsonBInclude != null && (this.full || !jsonBInclude.full());
   }
 
