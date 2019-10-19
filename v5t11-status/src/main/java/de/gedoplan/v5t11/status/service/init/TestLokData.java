e002eab77214d543fc9646253acea30f0b10796a
Dirk Weil <dirk.weil@gedoplan.de>
2019-10-19 20:57:07 +0200

    210 004-8 ergänzt

--- v5t11-status/src/main/java/de/gedoplan/v5t11/status/service/init/TestLokData.java
+++ v5t11-status/src/main/java/de/gedoplan/v5t11/status/service/init/TestLokData.java
@@ -44,6 +44,7 @@ public class TestLokData {
       new Lok.LokFunktion(33, Lok.LokFunktion.LokFunktionsGruppe.FL, "Doppel-A", false, false, 0b0000_0000_1010_0000, 0b0000_0000_1010_0000));
   public static final Lok lok151_073_4 = new Lok("151 073-4", "DH10", SystemTyp.SX1, false, 22, 31,
       new Lok.LokFunktion(1, Lok.LokFunktion.LokFunktionsGruppe.AF, "Horn", true, true));
+  public static final Lok lok210_004_8 = new Lok("X 210 004-8", "DHL100", SystemTyp.SX1, false, 2, 31);
   public static final Lok lok217_001_7 = new Lok("217 001-7", null, SystemTyp.SX2, false, 1217, 127,
       new Lok.LokFunktion(1, Lok.LokFunktion.LokFunktionsGruppe.AF, "Führerstandsbeleuchtung", false, false),
       new Lok.LokFunktion(2, Lok.LokFunktion.LokFunktionsGruppe.BG, "Motor", false, false),
@@ -123,7 +124,6 @@ public class TestLokData {
   // public static final Lok lokX_120_002_1 = new Lok("X 120 002-1", "Tr66830", SystemTyp.SX1, false, 18, 31);
   // public static final Lok lokX_14283 = new Lok("X 14283", "DHL050", SystemTyp.SX1, false, 3, 31);
   // public static final Lok lokX_151_032_0 = new Lok("X 151 032-0", "DHL100", SystemTyp.SX1, false, 20, 31);
-  // public static final Lok lokX_210_004_8 = new Lok("X 210 004-8", "DHL100", SystemTyp.SX1, false, 2, 31);
   // public static final Lok lokX_212_216_6 = new Lok("X 212 216-6", "Tr66825", SystemTyp.SX1, false, 5, 31);
   // public static final Lok lokX_221_137_3 = new Lok("X 221 137-3", "Tr66835", SystemTyp.SX1, false, 19, 31);
   // public static final Lok lokX_323_673_4 = new Lok("X 323 673-4", "DHL050", SystemTyp.SX1, false, 4, 31);
@@ -135,6 +135,7 @@ public class TestLokData {
       lok103_003_0,
       lok110_389_4,
       lok151_073_4,
+      lok210_004_8,
       lok217_001_7,
       lok230_001_0,
       lok430_119_8,
@@ -151,7 +152,6 @@ public class TestLokData {
       // lokX_14283,
       // lokX_151_032_0,
       // lokX_212_216_6,
-      // lokX_210_004_8,
       // lokX_221_137_3,
       // lokX_323_673_4,
       // lokX_332_262_5,
