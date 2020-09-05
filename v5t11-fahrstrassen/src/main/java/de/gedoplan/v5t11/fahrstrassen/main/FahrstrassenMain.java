ffa603d451e2515122a163db5591b1e2c10c39fa
dw <dirk.weil@gedoplan.de>
2020-09-05 17:09:48 +0200

    Main-Programme ergänzt

---
+++ v5t11-fahrstrassen/src/main/java/de/gedoplan/v5t11/fahrstrassen/main/FahrstrassenMain.java
@@ -0,0 +1,12 @@
+package de.gedoplan.v5t11.fahrstrassen.main;
+
+import io.quarkus.runtime.*;
+import io.quarkus.runtime.annotations.*;
+
+@QuarkusMain
+public class FahrstrassenMain {
+
+  public static void main(String[] args) {
+    Quarkus.run(args);
+  }
+}
