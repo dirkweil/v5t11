15b353a19c62fa666b99fb37316da3b91b6ab68a
Dirk Weil <dirk.weil@gedoplan.de>
2020-10-12 18:54:45 +0200

    Für JmsClient und StatusUpdater v5t11-util verwenden

--- v5t11-leitstand/src/main/java/de/gedoplan/v5t11/leitstand/service/StatusUpdater.java
+++ v5t11-leitstand/src/main/java/de/gedoplan/v5t11/leitstand/service/StatusUpdater.java
@@ -9,80 +9,56 @@ import de.gedoplan.v5t11.leitstand.entity.fahrweg.Signal;
 import de.gedoplan.v5t11.leitstand.entity.fahrweg.StatusUpdateable;
 import de.gedoplan.v5t11.leitstand.entity.fahrweg.Weiche;
 import de.gedoplan.v5t11.leitstand.entity.lok.Lok;
-import de.gedoplan.v5t11.leitstand.gateway.JmsClient;
 import de.gedoplan.v5t11.leitstand.gateway.StatusGateway;
 import de.gedoplan.v5t11.util.cdi.EventFirer;
 import de.gedoplan.v5t11.util.jms.MessageCategory;
 import de.gedoplan.v5t11.util.jsonb.JsonbWithIncludeVisibility;
+import de.gedoplan.v5t11.util.service.AbstractStatusUpdater;
 
+import java.util.EnumMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Consumer;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.naming.NamingException;
 
-import org.apache.commons.logging.Log;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @ApplicationScoped
-public class StatusUpdater implements Runnable {
-  private static final long RETRY_MILLIS = 10000;
-
-  @Inject
-  ConfigService configService;
+public class StatusUpdater extends AbstractStatusUpdater {
 
   @Inject
   @RestClient
   StatusGateway statusGateway;
 
   @Inject
-  JmsClient jmsClient;
-
-  @Inject
   FahrstrassenManager fahrstrassenManager;
-
-  @Inject
-  EventFirer eventFirer;
-
-  @Inject
-  Log log;
 
   @Inject
   Leitstand leitstand;
 
-  public void run() {
+  @Inject
+  EventFirer eventFirer;
 
-    while (true) {
-      try {
-        initializeStatus();
-
-        propagateStatus();
-      } catch (Exception e) {
-        String msg = "Fehler beim Status-Update (Status/Fahrstrassen-Service down?)";
-        if (this.log.isTraceEnabled()) {
-          this.log.warn(msg, e);
-        } else {
-          this.log.warn(msg);
-        }
-      }
-
-      try {
-        Thread.sleep(RETRY_MILLIS);
-      } catch (InterruptedException ie) {
-      }
-    }
+  @Override
+  protected Map<MessageCategory, Consumer<String>> getMessageHandler() {
+    Map<MessageCategory, Consumer<String>> messageHandler = new EnumMap<MessageCategory, Consumer<String>>(MessageCategory.class);
+    messageHandler.put(MessageCategory.FAHRSTRASSE, this::updateFahrstrasse);
+    messageHandler.put(MessageCategory.GLEIS, this::updateGleisabschnitt);
+    messageHandler.put(MessageCategory.LOKCONTROLLER, this::updateLokcontroller);
+    messageHandler.put(MessageCategory.GLEIS, this::updateGleisabschnitt);
+    messageHandler.put(MessageCategory.SIGNAL, this::updateSignal);
+    messageHandler.put(MessageCategory.WEICHE, this::updateWeiche);
+    messageHandler.put(MessageCategory.ZENTRALE, this::updateZentrale);
+    return messageHandler;
   }
 
   /*
    * Aktuelle Zustände von Gleisabschnitten holen
    */
-  private void initializeStatus() {
-    if (this.log.isDebugEnabled()) {
-      this.log.debug("Status initialisieren");
-    }
-
+  @Override
+  protected void initializeStatus() {
     this.statusGateway.getGleisabschnitte().forEach(statusGleisabschnitt -> {
       Gleisabschnitt gleisabschnitt = this.leitstand.getGleisabschnitt(statusGleisabschnitt.getBereich(), statusGleisabschnitt.getName());
       if (gleisabschnitt != null) {
@@ -130,71 +106,52 @@ public class StatusUpdater implements Runnable {
     });
   }
 
-  /*
-   * JMS-Meldungen zu Gleisbelegungsänderungen verarbeiten
-   */
-  private void propagateStatus() throws NamingException, JMSException {
-    if (this.log.isDebugEnabled()) {
-      this.log.debug("Status-Änderungen überwachen");
-    }
-
-    while (true) {
-      Message message = this.jmsClient.receive();
-      String text = message.getBody(String.class);
-      MessageCategory category = MessageCategory.valueOf(message.getStringProperty("category"));
-      switch (category) {
-      case FAHRSTRASSE:
-        Fahrstrasse statusFahrstrasse = JsonbWithIncludeVisibility.SHORT.fromJson(text, Fahrstrasse.class);
-        Fahrstrasse fahrstrasse = this.fahrstrassenManager.updateFahrstrasse(statusFahrstrasse);
-        if (fahrstrasse != null) {
-          if (this.log.isDebugEnabled()) {
-            this.log.debug(statusFahrstrasse);
-          }
-          this.eventFirer.fire(fahrstrasse);
-        }
-        break;
-
-      case GLEIS:
-        Gleisabschnitt statusGleisabschnitt = JsonbWithIncludeVisibility.SHORT.fromJson(text, Gleisabschnitt.class);
-        Gleisabschnitt gleisabschnitt = this.leitstand.getGleisabschnitt(statusGleisabschnitt.getBereich(), statusGleisabschnitt.getName());
-        if (gleisabschnitt != null) {
-          updateStatus(gleisabschnitt, statusGleisabschnitt);
-        }
-        break;
-
-      case LOKCONTROLLER:
-        LokController statusLokController = JsonbWithIncludeVisibility.SHORT.fromJson(text, LokController.class);
-        LokController lokController = this.leitstand.getLokController(statusLokController.getId());
-        if (lokController != null) {
-          updateStatus(lokController, statusLokController);
-        }
-        break;
-
-      case SIGNAL:
-        Signal statusSignal = JsonbWithIncludeVisibility.SHORT.fromJson(text, Signal.class);
-        Signal signal = this.leitstand.getSignal(statusSignal.getBereich(), statusSignal.getName());
-        if (signal != null) {
-          updateStatus(signal, statusSignal);
-        }
-        break;
-
-      case WEICHE:
-        Weiche statusWeiche = JsonbWithIncludeVisibility.SHORT.fromJson(text, Weiche.class);
-        Weiche weiche = this.leitstand.getWeiche(statusWeiche.getBereich(), statusWeiche.getName());
-        if (weiche != null) {
-          updateStatus(weiche, statusWeiche);
-        }
-        break;
-
-      case ZENTRALE:
-        Zentrale statusZentrale = JsonbWithIncludeVisibility.SHORT.fromJson(text, Zentrale.class);
-        updateStatus(this.leitstand.getZentrale(), statusZentrale);
-        break;
-
-      default:
-        this.log.warn("Status-Message mit unbekannter Category wird ignoriert: " + message);
+  private void updateFahrstrasse(String messageText) {
+    Fahrstrasse statusFahrstrasse = JsonbWithIncludeVisibility.SHORT.fromJson(messageText, Fahrstrasse.class);
+    Fahrstrasse fahrstrasse = this.fahrstrassenManager.updateFahrstrasse(statusFahrstrasse);
+    if (fahrstrasse != null) {
+      if (this.log.isDebugEnabled()) {
+        this.log.debug(statusFahrstrasse);
       }
+      this.eventFirer.fire(fahrstrasse);
     }
+  }
+
+  private void updateLokcontroller(String messageText) {
+    LokController statusLokController = JsonbWithIncludeVisibility.SHORT.fromJson(messageText, LokController.class);
+    LokController lokController = this.leitstand.getLokController(statusLokController.getId());
+    if (lokController != null) {
+      updateStatus(lokController, statusLokController);
+    }
+  }
+
+  private void updateGleisabschnitt(String messageText) {
+    Gleisabschnitt statusGleisabschnitt = JsonbWithIncludeVisibility.SHORT.fromJson(messageText, Gleisabschnitt.class);
+    Gleisabschnitt gleisabschnitt = this.leitstand.getGleisabschnitt(statusGleisabschnitt.getBereich(), statusGleisabschnitt.getName());
+    if (gleisabschnitt != null) {
+      updateStatus(gleisabschnitt, statusGleisabschnitt);
+    }
+  }
+
+  private void updateSignal(String messageText) {
+    Signal statusSignal = JsonbWithIncludeVisibility.SHORT.fromJson(messageText, Signal.class);
+    Signal signal = this.leitstand.getSignal(statusSignal.getBereich(), statusSignal.getName());
+    if (signal != null) {
+      updateStatus(signal, statusSignal);
+    }
+  }
+
+  private void updateWeiche(String messageText) {
+    Weiche statusWeiche = JsonbWithIncludeVisibility.SHORT.fromJson(messageText, Weiche.class);
+    Weiche weiche = this.leitstand.getWeiche(statusWeiche.getBereich(), statusWeiche.getName());
+    if (weiche != null) {
+      updateStatus(weiche, statusWeiche);
+    }
+  }
+
+  private void updateZentrale(String messageText) {
+    Zentrale statusZentrale = JsonbWithIncludeVisibility.SHORT.fromJson(messageText, Zentrale.class);
+    updateStatus(this.leitstand.getZentrale(), statusZentrale);
   }
 
   private <T extends StatusUpdateable<T>> void updateStatus(T object, T statusObject) {
