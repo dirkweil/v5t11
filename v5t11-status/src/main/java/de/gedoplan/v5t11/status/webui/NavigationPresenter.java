a5b4e8e07295b2183d6d6704d1d4830d8adca206
Dirk Weil <dirk.weil@gedoplan.de>
2020-10-14 19:10:26 +0200

    Web-UI auf Template mit Navigationsmenü umgestellt

---
+++ v5t11-status/src/main/java/de/gedoplan/v5t11/status/webui/NavigationPresenter.java
@@ -0,0 +1,73 @@
+package de.gedoplan.v5t11.status.webui;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.MenuModel;
+
+import lombok.Getter;
+
+@Named
+@ApplicationScoped
+public class NavigationPresenter {
+
+  @Getter
+  private MenuModel menuModel;
+
+  @PostConstruct
+  void postConstruct() {
+    this.menuModel = new DefaultMenuModel();
+
+    DefaultSubMenu statusSubmenu = DefaultSubMenu.builder()
+        .label("Status")
+        .build();
+
+    this.menuModel.getElements().add(statusSubmenu);
+
+    statusSubmenu.getElements().add(
+        DefaultMenuItem.builder()
+            .value("System-Status")
+            .url("http://localhost:8080/view/systemStatus.xhtml")
+            .icon("fa fa-info")
+            .build());
+
+    statusSubmenu.getElements().add(
+        DefaultMenuItem.builder()
+            .value("System-Control")
+            .url("http://localhost:8080/view/systemControl.xhtml")
+            .icon("fa fa-play")
+            .build());
+
+    statusSubmenu.getElements().add(
+        DefaultMenuItem.builder()
+            .value("Baustein-Programmierung")
+            .url("http://localhost:8080/view/bausteinProgrammierung.xhtml")
+            .icon("fa fa-cube")
+            .build());
+
+    DefaultSubMenu miscSubmenu = DefaultSubMenu.builder()
+        .label("Allgemein")
+        .build();
+
+    this.menuModel.getElements().add(miscSubmenu);
+
+    miscSubmenu.getElements().add(
+        DefaultMenuItem.builder()
+            .value("home")
+            .icon("fa fa-home")
+            .command("/index.xhtml")
+            .ajax(false)
+            .build());
+
+    miscSubmenu.getElements().add(
+        DefaultMenuItem.builder()
+            .value("refresh")
+            .icon("fa fa-refresh")
+            .ajax(false)
+            .build());
+  }
+}
