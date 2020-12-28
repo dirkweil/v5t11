720c2e3d570ce2cd2873135f533f913f0b7c31d0
Dirk Weil <dirk.weil@gedoplan.de>
2020-12-28 19:21:16 +0100

    UI zur Fahrzeugsteuerung implementiert (noch keine Push-Aktualisierung)

--- v5t11-fahrzeuge/src/main/java/de/gedoplan/v5t11/fahrzeuge/webui/FahrzeugPresenter.java
+++ v5t11-fahrzeuge/src/main/java/de/gedoplan/v5t11/fahrzeuge/webui/FahrzeugPresenter.java
@@ -4,15 +4,18 @@ import de.gedoplan.baselibs.utils.util.ResourceUtil;
 import de.gedoplan.v5t11.fahrzeuge.entity.fahrzeug.Fahrzeug;
 import de.gedoplan.v5t11.fahrzeuge.entity.fahrzeug.Fahrzeug.FahrzeugFunktion;
 import de.gedoplan.v5t11.fahrzeuge.entity.fahrzeug.Fahrzeug.FahrzeugFunktion.FahrzeugFunktionsGruppe;
+import de.gedoplan.v5t11.fahrzeuge.gateway.StatusGateway;
 import de.gedoplan.v5t11.fahrzeuge.persistence.FahrzeugRepository;
 import de.gedoplan.v5t11.util.domain.attribute.FahrzeugId;
 import de.gedoplan.v5t11.util.domain.attribute.SystemTyp;
 
 import java.io.Serializable;
+import java.text.Collator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
@@ -24,8 +27,10 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 import javax.validation.constraints.NotEmpty;
 
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -35,6 +40,10 @@ public class FahrzeugPresenter implements Serializable {
 
   @Inject
   FahrzeugRepository fahrzeugRepository;
+
+  @Inject
+  @RestClient
+  StatusGateway statusGateway;
 
   @Inject
   Logger log;
@@ -224,32 +233,121 @@ public class FahrzeugPresenter implements Serializable {
     return "control";
   }
 
-  @Getter
-  @Setter
-  boolean lokAktiv;
+  public Fahrzeug getRefreshedFahrzeug() {
+    if (!this.fahrzeugRepository.isAttached(this.currentFahrzeug)) {
+      this.currentFahrzeug = this.fahrzeugRepository.findById(this.currentFahrzeug.getId());
+    }
+    return this.currentFahrzeug;
+  }
 
-  @Getter
-  @Setter
-  boolean lokRueckwaerts;
+  public boolean isLokAktiv() {
+    return getRefreshedFahrzeug().isAktiv();
+  }
 
-  @Getter
-  @Setter
-  int lokFahrstufe;
+  public void setLokAktiv(boolean aktiv) {
+    this.statusGateway.changeFahrzeug(this.currentFahrzeug.getId(), aktiv, null, null, null, null);
+  }
+
+  public boolean isLokRueckwaerts() {
+    return getRefreshedFahrzeug().isRueckwaerts();
+  }
+
+  public void setLokRueckwaerts(boolean rueckwaerts) {
+    this.statusGateway.changeFahrzeug(this.currentFahrzeug.getId(), null, null, null, null, rueckwaerts);
+  }
+
+  public int getLokFahrstufe() {
+    return getRefreshedFahrzeug().getFahrstufe();
+  }
+
+  public void setLokFahrstufe(int fahrstufe) {
+    this.statusGateway.changeFahrzeug(this.currentFahrzeug.getId(), null, fahrstufe, null, null, null);
+  }
 
   public int getLokMaxFahrstufe() {
     return this.currentFahrzeug.getId().getSystemTyp().getMaxFahrstufe();
   }
 
-  public List<FahrzeugFunktion> getLokFunktionen(FahrzeugFunktionsGruppe fahrzeugFunktionsGruppe) {
-    return this.currentFahrzeug
-        .getFunktionen()
+  public List<FahrzeugFunktionsGruppe> getCurrentFunktionsGruppen() {
+    return this.currentFahrzeug.getFunktionen()
         .stream()
-        .filter(f -> f.getGruppe() == fahrzeugFunktionsGruppe)
+        .map(FahrzeugFunktion::getGruppe)
+        .sorted()
+        .distinct()
         .collect(Collectors.toList());
   }
 
-  @Getter
-  @Setter
-  boolean funktionAktiv;
+  public List<FahrzeugFunktionWrapper> getLokFunktionen(FahrzeugFunktionsGruppe fahrzeugFunktionsGruppe) {
+    Stream<FahrzeugFunktionWrapper> stream = this.currentFahrzeug
+        .getFunktionen()
+        .stream()
+        .filter(f -> f.getGruppe() == fahrzeugFunktionsGruppe)
+        .map(FahrzeugFunktionWrapper::new)
+        .sorted();
+    if (fahrzeugFunktionsGruppe == FahrzeugFunktionsGruppe.FL) {
+      stream = Stream.concat(Stream.of(this.lichtWrapper), stream);
+    }
+    return stream
+        .collect(Collectors.toList());
+  }
+
+  public FahrzeugFunktionWrapper getFahrzeugFunktionWrapper(FahrzeugFunktion fahrzeugFunktion) {
+    return new FahrzeugFunktionWrapper(fahrzeugFunktion);
+  }
+
+  @AllArgsConstructor
+  public class FahrzeugFunktionWrapper implements Comparable<FahrzeugFunktionWrapper> {
+    private FahrzeugFunktion fahrzeugFunktion;
+
+    public String getOnLabel() {
+      return this.fahrzeugFunktion.getBeschreibung();
+    }
+
+    public String getOffLabel() {
+      return this.fahrzeugFunktion.getBeschreibung();
+    }
+
+    public boolean isAktiv() {
+      return (getRefreshedFahrzeug().getFktBits() & this.fahrzeugFunktion.getMaske()) == this.fahrzeugFunktion.getWert();
+    }
+
+    public void setAktiv(boolean aktiv) {
+      int fktBits = getRefreshedFahrzeug().getFktBits() & (~this.fahrzeugFunktion.getMaske());
+      if (aktiv) {
+        fktBits |= this.fahrzeugFunktion.getWert();
+      }
+
+      FahrzeugPresenter.this.statusGateway.changeFahrzeug(FahrzeugPresenter.this.currentFahrzeug.getId(), null, null, fktBits, null, null);
+    }
+
+    @Override
+    public int compareTo(FahrzeugFunktionWrapper other) {
+      return Collator.getInstance().compare(this.fahrzeugFunktion.getBeschreibung(), other.fahrzeugFunktion.getBeschreibung());
+    }
+  }
+
+  private FahrzeugFunktionWrapper lichtWrapper = new FahrzeugFunktionWrapper(null) {
+
+    @Override
+    public String getOnLabel() {
+      return "an";
+    }
+
+    @Override
+    public String getOffLabel() {
+      return "aus";
+    }
+
+    @Override
+    public boolean isAktiv() {
+      return getRefreshedFahrzeug().isLicht();
+    }
+
+    @Override
+    public void setAktiv(boolean aktiv) {
+      FahrzeugPresenter.this.statusGateway.changeFahrzeug(FahrzeugPresenter.this.currentFahrzeug.getId(), null, null, null, aktiv, null);
+    }
+
+  };
 
 }
