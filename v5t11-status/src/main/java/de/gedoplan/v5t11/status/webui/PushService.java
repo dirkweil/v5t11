1c7d7c9df932ce2a4be1d715233fa05d88f6c8bb
Dirk Weil <dirk.weil@gedoplan.de>
2022-11-30 21:56:31 +0100

    Push-Service wieder zurück

--- v5t11-status/src/main/java/de/gedoplan/v5t11/status/webui/PushService.java
+++ v5t11-status/src/main/java/de/gedoplan/v5t11/status/webui/PushService.java
@@ -5,51 +5,71 @@ import de.gedoplan.v5t11.status.entity.fahrweg.geraet.Signal;
 import de.gedoplan.v5t11.status.entity.fahrweg.geraet.Weiche;
 import de.gedoplan.v5t11.status.entity.fahrzeug.Fahrzeug;
 import de.gedoplan.v5t11.util.cdi.Changed;
-import de.gedoplan.v5t11.util.jsf.UiRefreshService;
 import io.vertx.core.impl.ConcurrentHashSet;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
 import javax.enterprise.event.ObservesAsync;
-import javax.faces.event.WebsocketEvent;
-import javax.faces.push.Push;
-import javax.faces.push.PushContext;
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
-import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
+@ServerEndpoint("/javax.faces.push/uiRefresh")
 @ApplicationScoped
 public class PushService {
 
   @Inject
-  UiRefreshService uiRefreshService;
+  Logger logger;
 
   void fahrzeugChanged(@ObservesAsync @Changed Fahrzeug fahrzeug) {
-    this.uiRefreshService.send("fahrzeug");
+    send("fahrzeug");
   }
 
   void signalChanged(@ObservesAsync @Changed Signal signal) {
-    this.uiRefreshService.send("signal");
+    send("signal");
   }
 
   void weicheChanged(@ObservesAsync @Changed Weiche weiche) {
-    this.uiRefreshService.send("weiche");
+    send("weiche");
   }
 
   void zentraleChanged(@ObservesAsync @Changed Zentrale zentrale) {
-    this.uiRefreshService.send("zentrale");
+    send("zentrale");
   }
 
+  private Set<Session> sessions = new ConcurrentHashSet<>();
+
+  @OnOpen
+  void onOpen(Session session) {
+    sessions.add(session);
+    this.logger.debugf("Opened session %s", session);
+  }
+
+  @OnClose
+  void onClose(Session session) {
+    sessions.remove(session);
+    this.logger.debugf("Closed session %s", session);
+  }
+
+  @OnError
+  void onError(Session session, Throwable throwable) {
+    sessions.remove(session);
+    this.logger.debugf("Closed session %s because of %s", session, throwable);
+  }
+
+  private void send(String event) {
+    this.logger.debugf("Send refresh event %s to %d browser sessions", event, sessions.size());
+    String message = "{\"data\":\"" + event + "\"}";
+    sessions.forEach(s -> {
+      s.getAsyncRemote().sendText(message, result -> {
+        if (result.getException() != null) {
+          this.logger.error("Cannot send message", result.getException());
+        }
+      });
+    });
+  }
 }
