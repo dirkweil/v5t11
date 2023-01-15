a8723d3ece626f0b1b56417f93d984c928e7a7f7
Dirk Weil <dirk.weil@gedoplan.de>
2023-01-15 12:26:16 +0100

    Click auf UI-Elemente auf einzelne Presenter-Methoden abgebildet

--- v5t11-stellwerk/src/main/java/de/gedoplan/v5t11/leitstand/webui/StellwerkPresenter.java
+++ v5t11-stellwerk/src/main/java/de/gedoplan/v5t11/leitstand/webui/StellwerkPresenter.java
@@ -1,6 +1,13 @@
 package de.gedoplan.v5t11.leitstand.webui;
 
+import de.gedoplan.v5t11.leitstand.entity.fahrweg.Gleis;
+import de.gedoplan.v5t11.leitstand.entity.fahrweg.Signal;
+import de.gedoplan.v5t11.leitstand.entity.fahrweg.Weiche;
 import de.gedoplan.v5t11.leitstand.entity.stellwerk.Stellwerk;
+import de.gedoplan.v5t11.leitstand.entity.stellwerk.StellwerkDkw2;
+import de.gedoplan.v5t11.leitstand.entity.stellwerk.StellwerkEinfachWeiche;
+import de.gedoplan.v5t11.leitstand.entity.stellwerk.StellwerkElement;
+import de.gedoplan.v5t11.leitstand.entity.stellwerk.StellwerkGleis;
 import org.jboss.logging.Logger;
 
 import javax.annotation.PostConstruct;
@@ -34,6 +41,38 @@ public class StellwerkPresenter implements Serializable {
 
   public void elementClicked() {
     String uiId = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("uiId");
-    this.logger.debugf("stellwerkElementClicked on %s", uiId);
+    this.logger.tracef("stellwerkElementClicked on %s", uiId);
+
+    StellwerkElement element = this.stellwerkSessionHolder.getElementByUiUd(uiId);
+    if (element != null) {
+      this.logger.tracef("element: %s %s", element.getClass().getSimpleName(), element.getId());
+      if (element instanceof StellwerkGleis stellwerkGleis) {
+        gleisClicked(stellwerkGleis.findGleis());
+      } else if (element instanceof StellwerkEinfachWeiche stellwerkGleis) {
+        gleisClicked(stellwerkGleis.findGleis());
+        weicheClicked(stellwerkGleis.findWeiche());
+      } else if (element instanceof StellwerkDkw2 stellwerkGleis) {
+        gleisClicked(stellwerkGleis.findGleis());
+        weicheClicked(stellwerkGleis.findWeicheA());
+        weicheClicked(stellwerkGleis.findWeicheB());
+      }
+
+      Signal signal = element.findSignal();
+      if (signal != null) {
+        signalClicked(signal);
+      }
+    }
+  }
+
+  private void gleisClicked(Gleis gleis) {
+    this.logger.debugf("gleisClicked: %s", gleis.getId());
+  }
+
+  private void weicheClicked(Weiche weiche) {
+    this.logger.debugf("weicheClicked: %s", weiche.getId());
+  }
+
+  private void signalClicked(Signal signal) {
+    this.logger.debugf("signalClicked: %s", signal.getId());
   }
 }
--- v5t11-stellwerk/src/main/java/de/gedoplan/v5t11/leitstand/webui/StellwerkSessionHolder.java
+++ v5t11-stellwerk/src/main/java/de/gedoplan/v5t11/leitstand/webui/StellwerkSessionHolder.java
@@ -2,6 +2,7 @@ package de.gedoplan.v5t11.leitstand.webui;
 
 import de.gedoplan.v5t11.leitstand.entity.Leitstand;
 import de.gedoplan.v5t11.leitstand.entity.stellwerk.Stellwerk;
+import de.gedoplan.v5t11.leitstand.entity.stellwerk.StellwerkElement;
 import lombok.Getter;
 import org.jboss.logging.Logger;
 
@@ -9,6 +10,8 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import java.io.Serializable;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @SessionScoped
 public class StellwerkSessionHolder implements Serializable {
@@ -22,19 +25,22 @@ public class StellwerkSessionHolder implements Serializable {
   @Getter
   Stellwerk stellwerk;
 
+  private Map<String, StellwerkElement> uiId2StellwerkelementMap = Map.of();
+
   @PostConstruct
   void postConstruct() {
     this.stellwerk = this.leitstand.getStellwerke().first();
   }
 
   public void changeBereich(String bereich) {
+
     if (bereich == null) {
       return;
     }
 
-    if (this.stellwerk != null && this.stellwerk.getBereich().equals(bereich)) {
-      return;
-    }
+    //    if (this.stellwerk != null && this.stellwerk.getBereich().equals(bereich)) {
+    //      return;
+    //    }
 
     Stellwerk stellwerk = this.leitstand.getStellwerk(bereich);
     if (stellwerk == null) {
@@ -43,6 +49,17 @@ public class StellwerkSessionHolder implements Serializable {
 
     this.stellwerk = stellwerk;
 
-    this.logger.debugf("Neuer Bereich: %s", this.stellwerk.getBereich());
+    this.logger.debugf("Bereich: %s", this.stellwerk.getBereich());
+
+    this.uiId2StellwerkelementMap =
+      stellwerk
+        .getZeilen()
+        .stream()
+        .flatMap(x -> x.getElemente().stream())
+        .collect(Collectors.toMap(x -> x.getUiId(), x -> x));
+  }
+
+  public StellwerkElement getElementByUiUd(String uiId) {
+    return this.uiId2StellwerkelementMap.get(uiId);
   }
 }
