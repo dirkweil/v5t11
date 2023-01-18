713cbfe164f8ee66285407984495cbb71f774461
Dirk Weil <dirk.weil@gedoplan.de>
2023-01-18 08:44:17 +0100

    Controlpanel hinzugefügt

--- v5t11-stellwerk/src/main/java/de/gedoplan/v5t11/leitstand/webui/StellwerkPresenter.java
+++ v5t11-stellwerk/src/main/java/de/gedoplan/v5t11/leitstand/webui/StellwerkPresenter.java
@@ -8,6 +8,7 @@ import de.gedoplan.v5t11.leitstand.entity.stellwerk.StellwerkDkw2;
 import de.gedoplan.v5t11.leitstand.entity.stellwerk.StellwerkEinfachWeiche;
 import de.gedoplan.v5t11.leitstand.entity.stellwerk.StellwerkElement;
 import de.gedoplan.v5t11.leitstand.entity.stellwerk.StellwerkGleis;
+import lombok.Getter;
 import org.jboss.logging.Logger;
 
 import javax.annotation.PostConstruct;
@@ -26,6 +27,12 @@ public class StellwerkPresenter implements Serializable {
 
   @Inject
   Logger logger;
+
+  @Getter
+  private boolean controlPanelEnabled;
+
+  private Gleis startGleis = null;
+  private long gleisClickMillis = -1L;
 
   @PostConstruct
   void postConstruct() {
@@ -46,6 +53,10 @@ public class StellwerkPresenter implements Serializable {
     StellwerkElement element = this.stellwerkSessionHolder.getElementByUiUd(uiId);
     if (element != null) {
       this.logger.tracef("element: %s %s", element.getClass().getSimpleName(), element.getId());
+
+      this.controlPanelEnabled = false;
+      this.text = new StringBuilder();
+
       if (element instanceof StellwerkGleis stellwerkGleis) {
         gleisClicked(stellwerkGleis.findGleis());
       } else if (element instanceof StellwerkEinfachWeiche stellwerkGleis) {
@@ -66,13 +77,34 @@ public class StellwerkPresenter implements Serializable {
 
   private void gleisClicked(Gleis gleis) {
     this.logger.debugf("gleisClicked: %s", gleis.getId());
+
+    long now = System.currentTimeMillis();
+    if (this.gleisClickMillis >= 0L && (now - this.gleisClickMillis) < 3000) {
+      this.controlPanelEnabled = true;
+      this.text.append(this.startGleis.getName());
+      this.text.append("-");
+      this.text.append(gleis.getName());
+      this.text.append(" ");
+    } else {
+      this.gleisClickMillis = now;
+      this.startGleis = gleis;
+    }
   }
 
   private void weicheClicked(Weiche weiche) {
     this.logger.debugf("weicheClicked: %s", weiche.getId());
+    this.controlPanelEnabled = true;
+    this.text.append(weiche.getName());
+    this.text.append(" ");
   }
 
   private void signalClicked(Signal signal) {
     this.logger.debugf("signalClicked: %s", signal.getId());
+    this.controlPanelEnabled = true;
+    this.text.append(signal.getName());
+    this.text.append(" ");
   }
+
+  @Getter
+  private StringBuilder text = new StringBuilder();
 }
