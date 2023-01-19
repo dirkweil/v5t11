a31b092c6e8e4d43967b79a55d36658d226a2bbd
Dirk Weil <dirk.weil@gedoplan.de>
2023-01-19 10:04:38 +0100

    Controlpanel hinzugefügt

--- v5t11-stellwerk/src/main/java/de/gedoplan/v5t11/leitstand/webui/StellwerkPresenter.java
+++ v5t11-stellwerk/src/main/java/de/gedoplan/v5t11/leitstand/webui/StellwerkPresenter.java
@@ -8,6 +8,7 @@ import de.gedoplan.v5t11.leitstand.entity.stellwerk.StellwerkDkw2;
 import de.gedoplan.v5t11.leitstand.entity.stellwerk.StellwerkEinfachWeiche;
 import de.gedoplan.v5t11.leitstand.entity.stellwerk.StellwerkElement;
 import de.gedoplan.v5t11.leitstand.entity.stellwerk.StellwerkGleis;
+import de.gedoplan.v5t11.util.domain.attribute.WeichenStellung;
 import lombok.Getter;
 import org.jboss.logging.Logger;
 
@@ -34,6 +35,15 @@ public class StellwerkPresenter implements Serializable {
   private Gleis startGleis = null;
   private long gleisClickMillis = -1L;
 
+  @Getter
+  private Weiche weiche;
+
+  @Getter
+  private Weiche weiche2;
+
+  @Getter
+  private Signal signal;
+
   @PostConstruct
   void postConstruct() {
     String bereich = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("bereich");
@@ -54,18 +64,16 @@ public class StellwerkPresenter implements Serializable {
     if (element != null) {
       this.logger.tracef("element: %s %s", element.getClass().getSimpleName(), element.getId());
 
-      this.controlPanelEnabled = false;
-      this.text = new StringBuilder();
+      clearControlPanel();
 
       if (element instanceof StellwerkGleis stellwerkGleis) {
         gleisClicked(stellwerkGleis.findGleis());
       } else if (element instanceof StellwerkEinfachWeiche stellwerkGleis) {
         gleisClicked(stellwerkGleis.findGleis());
-        weicheClicked(stellwerkGleis.findWeiche());
+        weicheClicked(stellwerkGleis.findWeiche(), null);
       } else if (element instanceof StellwerkDkw2 stellwerkGleis) {
         gleisClicked(stellwerkGleis.findGleis());
-        weicheClicked(stellwerkGleis.findWeicheA());
-        weicheClicked(stellwerkGleis.findWeicheB());
+        weicheClicked(stellwerkGleis.findWeicheA(), stellwerkGleis.findWeicheB());
       }
 
       Signal signal = element.findSignal();
@@ -73,6 +81,14 @@ public class StellwerkPresenter implements Serializable {
         signalClicked(signal);
       }
     }
+  }
+
+  public void clearControlPanel() {
+    this.controlPanelEnabled = false;
+    this.weiche = null;
+    this.weiche2 = null;
+    this.signal = null;
+    this.text = new StringBuilder();
   }
 
   private void gleisClicked(Gleis gleis) {
@@ -91,18 +107,25 @@ public class StellwerkPresenter implements Serializable {
     }
   }
 
-  private void weicheClicked(Weiche weiche) {
+  private void weicheClicked(Weiche weiche, Weiche weiche2) {
     this.logger.debugf("weicheClicked: %s", weiche.getId());
+
+    this.weiche = weiche;
+    this.weiche2 = weiche2;
+
     this.controlPanelEnabled = true;
-    this.text.append(weiche.getName());
-    this.text.append(" ");
+  }
+
+  public WeichenStellung[] getWeichenStellungen() {
+    return WeichenStellung.values();
   }
 
   private void signalClicked(Signal signal) {
     this.logger.debugf("signalClicked: %s", signal.getId());
+
+    this.signal = signal;
+
     this.controlPanelEnabled = true;
-    this.text.append(signal.getName());
-    this.text.append(" ");
   }
 
   @Getter
