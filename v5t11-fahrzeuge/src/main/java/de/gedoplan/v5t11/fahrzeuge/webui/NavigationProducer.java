c534991d744403e46495257fe533796665f7ec1f
Dirk Weil <dirk.weil@gedoplan.de>
2025-09-21 19:27:23 +0200

    Fahrzeug-Kacheln klickbar

--- v5t11-fahrzeuge/src/main/java/de/gedoplan/v5t11/fahrzeuge/webui/NavigationProducer.java
+++ v5t11-fahrzeuge/src/main/java/de/gedoplan/v5t11/fahrzeuge/webui/NavigationProducer.java
@@ -3,13 +3,13 @@ package de.gedoplan.v5t11.fahrzeuge.webui;
 import de.gedoplan.v5t11.fahrzeuge.service.ConfigService;
 import de.gedoplan.v5t11.util.jsf.NavigationItem;
 
+import java.util.List;
+
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
-
-import java.util.List;
 
 @Dependent
 public class NavigationProducer {
@@ -27,6 +27,9 @@ public class NavigationProducer {
   @Produces
   @ApplicationScoped
   List<NavigationItem> getFahrzeugManagementNavigationItem() {
-    return List.of(new NavigationItem("Fahrzeug-Management", "Fahrzeuge", this.urlPrefix + "view/fahrzeugManagement.xhtml", "pi pi-th-large", 210));
+    return List.of(
+      new NavigationItem("Fahrzeug-Management", "Fahrzeuge", this.urlPrefix + "view/fahrzeugManagement.xhtml", "pi pi-th-large", 210),
+      new NavigationItem("Fahrzeug-Management (alt)", "Fahrzeuge", this.urlPrefix + "view/fahrzeugManagement_alt.xhtml", "pi pi-th-large", 211)
+    );
   }
 }
