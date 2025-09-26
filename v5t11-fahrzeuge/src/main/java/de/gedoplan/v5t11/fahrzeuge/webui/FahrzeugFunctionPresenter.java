e67ac243eb54f4ad6a2b669ef8cc2fc30baf3655
Dirk Weil <dirk.weil@gedoplan.de>
2025-09-26 12:17:11 +0200

    UI überarbeitet: Fahrzeugfunktionen

--- v5t11-fahrzeuge/src/main/java/de/gedoplan/v5t11/fahrzeuge/webui/FahrzeugFunctionPresenter.java
+++ v5t11-fahrzeuge/src/main/java/de/gedoplan/v5t11/fahrzeuge/webui/FahrzeugFunctionPresenter.java
@@ -3,81 +3,44 @@ package de.gedoplan.v5t11.fahrzeuge.webui;
 import de.gedoplan.v5t11.fahrzeuge.entity.fahrzeug.Fahrzeug;
 import de.gedoplan.v5t11.fahrzeuge.entity.fahrzeug.FahrzeugFunktion;
 import de.gedoplan.v5t11.fahrzeuge.entity.fahrzeug.FahrzeugFunktion.FahrzeugFunktionsGruppe;
-import de.gedoplan.v5t11.fahrzeuge.persistence.FahrzeugRepository;
-import de.gedoplan.v5t11.util.cdi.Current;
 
 import java.io.Serializable;
 import java.util.Iterator;
-import java.util.Set;
 
-import jakarta.faces.application.FacesMessage;
-import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validator;
-
-import org.jboss.logging.Logger;
-
-import lombok.Getter;
 
 @Named
 @ViewScoped
-public class FahrzeugEditPresenter implements Serializable {
+public class FahrzeugFunctionPresenter implements Serializable {
 
   @Inject
-  @Current
-  @Getter
-  Fahrzeug currentFahrzeug;
+  FahrzeugListPresenter fahrzeugListPresenter;
 
-  @Inject
-  FahrzeugRepository fahrzeugRepository;
+  //  @Inject
+  //  Logger log;
 
-  @Inject
-  Logger log;
+  public Fahrzeug getCurrentFahrzeug() {
+    return this.fahrzeugListPresenter.getCurrentFahrzeug();
+  }
 
-  @Inject
-  Validator validator;
+  public FahrzeugFunktionsGruppe[] getFunktionsGruppen() {
+    return FahrzeugFunktionsGruppe.values();
+  }
 
   public void addFunktion() {
-    this.currentFahrzeug.getFunktionen().add(0, new FahrzeugFunktion(FahrzeugFunktionsGruppe.AF, 0, 0, false, false, false, ""));
+    getCurrentFahrzeug().getFunktionen().add(0, new FahrzeugFunktion(FahrzeugFunktionsGruppe.AF, 0, 0, false, false, false, ""));
   }
 
   public void removeFunktion(FahrzeugFunktion funktion) {
-    Iterator<FahrzeugFunktion> iterator = this.currentFahrzeug.getFunktionen().iterator();
+    Iterator<FahrzeugFunktion> iterator = getCurrentFahrzeug().getFunktionen().iterator();
     while (iterator.hasNext()) {
       if (iterator.next() == funktion) {
         iterator.remove();
         break;
       }
     }
-  }
-
-  public String save() {
-    Set<ConstraintViolation<Fahrzeug>> violations = this.validator.validate(this.currentFahrzeug);
-    if (!violations.isEmpty()) {
-      FacesContext facesContext = FacesContext.getCurrentInstance();
-      violations.forEach(cv -> {
-        FacesMessage facesMessage = new FacesMessage(cv.getMessage());
-        facesMessage.setSeverity(FacesMessage.SEVERITY_ERROR);
-        facesContext.addMessage(null, facesMessage);
-      });
-      facesContext.validationFailed();
-      return null;
-    }
-
-    Iterator<FahrzeugFunktion> iterator = this.currentFahrzeug.getFunktionen().iterator();
-    while (iterator.hasNext()) {
-      FahrzeugFunktion funktion = iterator.next();
-      if (funktion.getMaske() == 0 || funktion.getBeschreibung() == null || funktion.getBeschreibung().strip().isEmpty()) {
-        iterator.remove();
-      }
-    }
-    this.fahrzeugRepository.merge(this.currentFahrzeug);
-
-    return "finished";
-
   }
 
   public TriStateAdapter getTriStateAdapter(FahrzeugFunktion fahrzeugFunktion, int bitNr) {
